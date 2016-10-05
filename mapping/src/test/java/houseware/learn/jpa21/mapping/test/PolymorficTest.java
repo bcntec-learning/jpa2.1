56da98582316158419077926caa0989e8100edcf
Francisco Philip <fphilip@houseware.es>
2016-10-05 22:34:43 +0200

    polymorfic

--- mapping/src/test/java/houseware/learn/jpa21/mapping/test/PolymorficTest.java
+++ mapping/src/test/java/houseware/learn/jpa21/mapping/test/PolymorficTest.java
@@ -1,5 +1,6 @@
 package houseware.learn.jpa21.mapping.test;
 
+import houseware.learn.jpa21.mapping.polymorphic.Animal;
 import houseware.learn.jpa21.mapping.secondaryTable.User;
 import houseware.learn.jpa21.mapping.secondaryTable.UserFull;
 import lombok.Cleanup;
@@ -9,6 +10,9 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  * @author fphilip@houseware.es
@@ -24,6 +28,24 @@ public class PolymorficTest {
 
         em.createQuery("select object (a) from Animal a ").getResultList();
         em.createQuery("select object (a) from Dog a ").getResultList();
+
+    }
+
+    @Test
+    public void polymorphic_criteria() {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa21:polymorphic");
+        @Cleanup
+        EntityManager em = emf.createEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Animal> cq = cb.createQuery(Animal.class);
+
+        Root<Animal> r = cq.from(Animal.class);
+        cq.select(r);
+
+        em.createQuery(cq).getResultList();
+
 
     }
 
